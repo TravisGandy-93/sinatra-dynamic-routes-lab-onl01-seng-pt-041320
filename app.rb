7571e01ba5ca368312747970753dd62874f0db6e
Travis Gandy <travis_gandy@yahoo.com>
2020-08-02 17:12:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,5 +13,8 @@ class App < Sinatra::Base
   end 
   
   get '/say/:number/:phrase' do
-    
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@phrase.times(@num)}"
+  end 
 end
