3be3b7f028d3bb29da129360b426060c35d14d21
Travis Gandy <travis_gandy@yahoo.com>
2020-08-02 17:21:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,5 +27,18 @@ class App < Sinatra::Base
     "#{@word1 + " " + @word2 + " " + @word3 + " " + @word4 + " " + @word5 + "."}"
    end
    
-   
+    get "/:operation/:num1/:num2" do
+      @num1 = params[:num1].to_i
+      @num2 = params[:num2].to_i
+      @operation = params[:operation]
+      if params[:operation] == "multiply"
+      "#{@num1 * @num2}"
+      elsif params[:operation] == "add"
+      "#{@num1 + @num2}"
+      elsif params[:operation] == "divide"
+        "#{@num1 / @num2}"
+      elsif params[:operation] == "subtract"
+        "#{@num1 - @num2}"
+      end
+    end
 end
